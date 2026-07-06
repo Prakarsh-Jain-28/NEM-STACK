1d3edd507afcf6d56bfe698a21639c44b034d7fd
Prakarsh Jain <prakarshj51@gmail.com>
2026-07-06 05:57:59 +0530

    Connecting Backend with EJS Lec 22

---
+++ Lec 22 SSR/controllers/url.js
@@ -0,0 +1,80 @@
+const shortid = require("shortid");
+const URL = require("../models/url");
+
+async function PostUrl(req, res) {
+    const body = req.body;
+
+    if (!body.url) {
+        return res.status(400).json({
+            status: "Error",
+            message: "Enter Valid URL",
+        });
+    }
+
+    const existing = await URL.findOne({
+        redirectURL: body.url,
+    });
+
+    if (existing) {
+        const allUrls = await URL.find({});
+
+        return res.render("home", {
+            urls: allUrls,
+            id: existing.shortId,
+            originalURL: existing.redirectURL,
+            port: process.env.PORT,
+        });
+    }
+
+    const id = shortid();
+
+    const result = await URL.create({
+        redirectURL: body.url,
+        shortId: id,
+        visitHistory: [],
+    });
+
+    const allUrls = await URL.find({});
+
+    return res.render("home", {
+        urls: allUrls,
+        id: result.shortId,
+        originalURL: result.redirectURL,
+        port: process.env.PORT,
+    });
+}
+async function GetAnalytics(req,res)
+{
+    const id = req.params.id;
+    console.log(id);
+    const result = await URL.findOne({shortId: id});
+    return res.json({
+        Clicks: result.visitHistory.length,
+        Analytics: result.visitHistory,
+    })
+}
+
+async function GetId(req, res) {
+
+    const shortId = req.params.id;
+
+    const entry = await URL.findOneAndUpdate(
+        { shortId: shortId },
+        {
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now(),
+                },
+            },
+        },
+        { returnDocument: "after" }
+    );
+
+    if (!entry) {
+        return res.status(404).send("Short URL not found");
+    }
+
+    return res.redirect(entry.redirectURL);
+}
+
+module.exports = {PostUrl,GetAnalytics,GetId};
